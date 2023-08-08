73f67162ac7a78ae432d6735b49cfd0186b7ba32
charleseduardome <charleseduardo.me@gmail.com>
2023-08-08 19:31:37 -0300

    feat: add shorts videos

--- src/utils/getDataVideo.ts
+++ src/utils/getDataVideo.ts
@@ -1,6 +1,7 @@
 export const getDataVideo = () => {
   const input = document.getElementById("input-url-video-to-transcription") as HTMLInputElement;
-  const id = input.value.split('watch?v=')[1];
+  const id = input.value.split('watch?v=')[1] || input.value.split('shorts/')[1];
+
   const url = input.value;
 
   return { id, url }
