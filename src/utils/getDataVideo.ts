ba429c767b248eaf0ea9d3eeb6582a9416bb5490
charleseduardome <charleseduardo.me@gmail.com>
2023-09-05 19:38:44 -0300

    fix: url invalid

--- src/utils/getDataVideo.ts
+++ src/utils/getDataVideo.ts
@@ -1,12 +1,22 @@
 export const getDataVideo = () => {
+  const YtRegex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi;
   const input = document.getElementById("input-url-video-to-transcription") as HTMLInputElement;
+
   const value = 
     input.value.split('?feature=share').length 
     ? input.value.split('?feature=share')[0] 
     : input.value
+
   
-  if (value && (value.includes('watch?v=') || value.includes('shorts/'))) {
-    const id = input.value.split('watch?v=')[1] || input.value.split('shorts/')[1];
+  if (value && (value.includes('watch?v='))) {
+    const filteredURL = String(input.value.match(YtRegex))
+    const id = filteredURL?.split('watch?v=')[1];
+
+    return { id }
+  }
+
+  if (value && value.includes('shorts/')) {
+    const id = input.value.split('shorts/')[1];
 
     return { id }
   }
