0eac4bdd0d0b1227be5dc4671ad4d63c2f90739a
Charles Eduardo <charleseduardo.me@gmail.com>
2023-09-10 23:36:57 -0300

    remove cached file

---
+++ src/components/Speech/index.tsx
@@ -0,0 +1,56 @@
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+interface ISpeechProps {
+  appId: string
+}
+
+export default function Speech({ appId }:ISpeechProps) {
+  const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+  SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
+
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+    resetTranscript,
+  } = useSpeechRecognition();
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-US" });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser `&lsquo;` support speech recognition.</span>;
+  }
+
+  if (!isMicrophoneAvailable) {
+    return <span>You not acessed the microphone.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button
+        onTouchStart={startListening}
+        onMouseDown={startListening}
+        onTouchEnd={SpeechRecognition.stopListening}
+        onMouseUp={SpeechRecognition.stopListening}
+      >
+        Hold to talk
+      </button>
+      <button
+        onTouchStart={resetTranscript}
+        onMouseDown={resetTranscript}
+        onTouchEnd={resetTranscript}
+        onMouseUp={resetTranscript}
+      >
+        Reset
+      </button>
+
+      <p>{transcript}</p>
+    </div>
+  );
+}
