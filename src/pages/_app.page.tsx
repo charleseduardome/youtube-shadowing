1a985cf51a912aeb590e9204f272baa5ac3b0ea1
Charles Eduardo <charleseduardo.me@gmail.com>
2023-10-17 21:11:19 -0300

    fix: handle viewport safari

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,12 +1,19 @@
 import "regenerator-runtime/runtime";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head'
 
 import AppProvider from '../providers'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <AppProvider>
-    <Component {...pageProps} />
-  </AppProvider>)
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </>
+  )
 }
