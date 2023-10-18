cddfe06c258f54f8158a4deef31c5b0bbde7fed2
Charles Eduardo <charleseduardo.me@gmail.com>
2023-10-17 21:14:26 -0300

    fix: handle viewport safari

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </Head>
       <AppProvider>
         <Component {...pageProps} />
